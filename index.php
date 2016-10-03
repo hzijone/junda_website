be29e4f7ef9077119a9e6e686644371f6b48dd30
h659742940@126.com <h659742940@126.com>
2016-10-03 20:07:59 +0800

    for company website

---
+++ index.php
@@ -0,0 +1,122 @@
+<html>
+<head>
+<style type="text/css" media="screen">
+
+.nav > li .dropdown-menu {
+margin: 0;
+}
+.nav > li:hover .dropdown-menu {
+display: block;
+}
+
+
+</style>
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>竣达技术-UPS监控技术专家</title>
+    <link rel="icon" href="images/favicon.ico" type="image/x-ico">
+    <script src="extlib/jquery-1.11.1.min.js" type="text/javascript" charset="utf-8"></script>
+    <link rel="stylesheet" href="extlib/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+    <script src="extlib/bootstrap-3.3.5-dist/js/bootstrap.min.js" type="text/javascript" charset="utf-8" ></script>
+    <script src="extlib/bootstrap-3.3.5-dist/js/tooltip.js" type="text/javascript" charset="utf-8" ></script>
+    <script src="extlib/bootstrap-3.3.5-dist/js/carousel.js" type="text/javascript" charset="utf-8" ></script>
+    <link rel="stylesheet" type="text/css" href="extlib/font-awesome.min.css">
+</head>
+<body>
+    <div class="container">
+    <?php
+require_once "header.php";
+?>
+     <br>
+     <!-- &nbsp; -->
+    <!-- 图片轮播 -->
+     <div class="row">
+     <div class="col-sm-offset-1 col-sm-10">
+     <div id="myCarousel" class="carousel slide">
+         <ol class="carousel-indicators">
+             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+             <li data-target="#myCarousel" data-slide-to="1"></li>
+             <li data-target="#myCarousel" data-slide-to="2"></li>
+         </ol>
+
+          <!-- Carousel items -->
+        <div class="carousel-inner">
+            <div class="active item">
+                <a href="usercase.php" title="" target="_blank">
+                <img src="images/banner-201308191512580.jpg" height="409" width="1000" alt="">
+                </a>
+            </div>
+            <div class="item">
+                <img src="images/banner-201308191607480.jpg" height="409" width="1000" alt="">
+            </div>
+            <div class="item">
+                <img src="images/banner-201308221138340.jpg" height="409" width="1000" alt="">
+            </div>
+        </div>
+
+         <!-- Carousel nav -->
+         <a href="#myCarousel" class="carousel-control left" data-slide="prev" role="button">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+         </a>
+         <a href="#myCarousel" class="carousel-control right" data-slide="next" role="buttonu">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+         </a>
+
+     </div>
+     </div>
+     </div>
+
+      <br>
+    <!-- 首页内容 -->
+    <div class="row">
+        <div class="col-sm-10">
+        <h3>广州市竣达智能软件技术有限公司</h3>由一批专注于 UPS 监控技术的专业技术人员共同发起成立，以发展UPS监控技术作为公司的专业定位，公司以“专注方能专业”作为未来发展的首要策略，为广大的UPS电源生产厂家，系统集成等客户提供配套的产品及技术服务。<br>
+        </div>
+
+        <img src="images/product_desc.jpg" height="452" width="925" alt=""><br>
+        <div class="col-sm-10">
+
+        在过去的十多年的时间里，研发团队多次和国内外知名厂商合作，开发完成一系列的UPS液晶显示，通讯接口产品，包括RS232，RS485，USB等配套方 案，推出多款UPS的网络监控适配器（SNMP卡），MODBUS协议转换，涵盖了UPS从单机监控到网络监控的全系列产品，积累了丰富的UPS集中监控，管理经验，在跨品牌跨平台方面，也有了充足的技术储备和积累。<br>
+        </div>
+      </div>
+        <br>
+
+     <div class="row">
+        <div class="col-sm-10">
+        <h4>竣达公司大事记</h5>
+        <ul>
+        <li> 1998年，我们配合合作伙伴，设计第一款基于16F71的UPS液晶控制方案</li>
+        <li>2000年，基于89C52的液晶控制接口正式在客户机型中进入量产，并在后续的时间里得到广泛应用。</li>
+        <li> 2001年，完成设计并发布国内自主设计的UPS SNMP卡及配套软件：Netmate</li>
+        <li> 2004年，完成设计并发布第二代UPS SNMP卡：Netmate II</li>
+        <li> 2005年，发布了NETMATE EXT系列产品，并提供标准1U式结构，并开始研制经济性电池巡检系统</li>
+        <li> 2007年，提供全面的机房监控解决方案，进一步升级完善了配套产品</li>
+        <li> 2008年，实现了自助银行集中监控系统整合。</li>
+        <li>2009年，电池巡检技术取得重大突破，完成电池巡检系列产品，不间断电源USB接口方案升级</li>
+        <li> 2010年，整合产品．进一步完善机房整体方案． </li>
+        <li> 2012年,  综合升级发布 UPS机房环境综合监控系统软件: PmCenter </li>
+        <li>  2013年,  完成三相SPWM控制模块设计定型并批量供货. 完成不间断电源人际显示面板设计项目</li>
+        </ul>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+</div>
+<?php
+require_once "footer.php";
+?>
+</body>
+</html>
---
+++ upload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git add .
+git commit -am "add adoc file to summary"
+git push origin master:master
